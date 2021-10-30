f5069c87780e6bafab5812a4a03463d1ee96d1b8
cgonzalezdev <cgonzalez.dev@gmail.com>
2021-10-30 14:27:44 -0500

    Modo Producer

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Configuration of the server
-USE_RELOADER = True
-THREADED = True
+USE_RELOADER = False
+THREADED = False
 HOST = "0.0.0.0"
-DEBUG = True
+DEBUG = False
 
 # Name database
 NAME_DATABASE = "dbECommerce.db"
